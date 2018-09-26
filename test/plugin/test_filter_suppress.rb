d1dc336fcc9f064915e243dce03c9a28fea2bd57
Eiichi Tsukata <devel@etsukata.com>
2018-09-26 14:11:30 +0900

    Add `max_slot_num` param to limit memory usage
    Currently, the `@slots` length can grow without any limitation when
    cardinarity of `attr_key` is large enough.
    
    Introduces `max_slot_num` param to put an upper limit on the `@slots`
    length to control memory usage.

--- test/plugin/test_filter_suppress.rb
+++ test/plugin/test_filter_suppress.rb
@@ -26,6 +26,13 @@ class SuppressFilterTest < Test::Unit::TestCase
     num      2
   ]
 
+  CONFIG_MAX_SLOT_NUM = %[
+    interval       10
+    num            2
+    max_slot_num   3
+    attr_keys      host, message
+  ]
+
   def create_driver(conf = CONFIG)
     Fluent::Test::Driver::Filter.new(Fluent::Plugin::SuppressFilter).configure(conf)
   end
@@ -108,5 +115,31 @@ class SuppressFilterTest < Test::Unit::TestCase
     assert_equal({"id"=>2, "host"=>"web02", "message"=>"2 error!!"}, records[1])
     assert_equal({"id"=>6, "host"=>"web06", "message"=>"6 error!!"}, records[2])
     assert_equal({"id"=>7, "host"=>"web07", "message"=>"7 error!!"}, records[3])
+  end
+
+  def test_emit_max_slot_num
+    d = create_driver(CONFIG_MAX_SLOT_NUM)
+    es = Fluent::MultiEventStream.new
+
+    time = event_time("2012-11-22 11:22:33 UTC")
+    es.add(time + 1,  {"id" => 1, "host" => "web01", "message" => "1 error!!"})
+    es.add(time + 2,  {"id" => 2, "host" => "web02", "message" => "2 error!!"})
+    es.add(time + 3,  {"id" => 3, "host" => "web03", "message" => "3 error!!"})
+    es.add(time + 4,  {"id" => 4, "host" => "web01", "message" => "1 error!!"})
+    es.add(time + 5,  {"id" => 5, "host" => "web04", "message" => "4 error!!"})
+    es.add(time + 6,  {"id" => 6, "host" => "web01", "message" => "1 error!!"})
+
+    d.run(default_tag: "test.info") do
+      d.feed(es)
+    end
+    records = d.filtered_records
+
+    assert_equal 6, records.length
+    assert_equal({"id"=>1, "host"=>"web01", "message"=>"1 error!!"}, records[0])
+    assert_equal({"id"=>2, "host"=>"web02", "message"=>"2 error!!"}, records[1])
+    assert_equal({"id"=>3, "host"=>"web03", "message"=>"3 error!!"}, records[2])
+    assert_equal({"id"=>4, "host"=>"web01", "message"=>"1 error!!"}, records[3])
+    assert_equal({"id"=>5, "host"=>"web04", "message"=>"4 error!!"}, records[4])
+    assert_equal({"id"=>6, "host"=>"web01", "message"=>"1 error!!"}, records[5])
   end
 end
